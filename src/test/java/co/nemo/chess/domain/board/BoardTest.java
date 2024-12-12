9c315ef4ab8230ef55c53b2a3692531d899e526b
yonghwankim-dev <dragonbead95@naver.com>
2024-12-12 13:18:09 +0900

    test: 테스트 메서드 수정

--- src/test/java/co/nemo/chess/domain/board/BoardTest.java
+++ src/test/java/co/nemo/chess/domain/board/BoardTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import co.nemo.chess.domain.piece.AbstractChessPiece;
 import co.nemo.chess.domain.piece.Color;
 import co.nemo.chess.domain.piece.Location;
 import co.nemo.chess.domain.piece.Piece;
@@ -36,18 +37,24 @@ class BoardTest {
 	}
 
 	public static Stream<Arguments> validPawnMoveLocations() {
-		Piece a2WhitePawn = PieceFactory.getInstance().pawn("a2", Color.WHITE);
-		Piece a7DarkPawn = PieceFactory.getInstance().pawn("a7", Color.DARK);
+		AbstractChessPiece a2WhitePawn = PieceFactory.getInstance().pawn("a2", Color.WHITE);
+		AbstractChessPiece a7DarkPawn = PieceFactory.getInstance().pawn("a7", Color.DARK);
 
-		Piece a3WhitePawn = PieceFactory.getInstance().pawn("a3", Color.WHITE).withMoved();
-		Piece a4WhitePawn = PieceFactory.getInstance().pawn("a4", Color.WHITE).withMoved();
-		Piece a5DarkPawn = PieceFactory.getInstance().pawn("a5", Color.DARK).withMoved();
-		Piece a6DarkPawn = PieceFactory.getInstance().pawn("a6", Color.DARK).withMoved();
+		AbstractChessPiece a3WhitePawn = PieceFactory.getInstance().pawn("a3", Color.WHITE).withMoved();
+		AbstractChessPiece a4WhitePawn = PieceFactory.getInstance().pawn("a4", Color.WHITE).withMoved();
+		AbstractChessPiece a5DarkPawn = PieceFactory.getInstance().pawn("a5", Color.DARK).withMoved();
+		AbstractChessPiece a6DarkPawn = PieceFactory.getInstance().pawn("a6", Color.DARK).withMoved();
+		Location a2Location = Location.from("a2");
+		Location a3Location = Location.from("a3");
+		Location a4Location = Location.from("a4");
+		Location a5Location = Location.from("a5");
+		Location a6Location = Location.from("a6");
+		Location a7Location = Location.from("a7");
 		return Stream.of(
-			Arguments.of(a2WhitePawn, "a2", "a3", a3WhitePawn),
-			Arguments.of(a2WhitePawn, "a2", "a4", a4WhitePawn),
-			Arguments.of(a7DarkPawn, "a7", "a5", a5DarkPawn),
-			Arguments.of(a7DarkPawn, "a7", "a6", a6DarkPawn)
+			Arguments.of(new AbstractChessPiece[] {a2WhitePawn}, a2Location, a3Location, a3WhitePawn),
+			Arguments.of(new AbstractChessPiece[] {a2WhitePawn}, a2Location, a4Location, a4WhitePawn),
+			Arguments.of(new AbstractChessPiece[] {a7DarkPawn}, a7Location, a5Location, a5DarkPawn),
+			Arguments.of(new AbstractChessPiece[] {a7DarkPawn}, a7Location, a6Location, a6DarkPawn)
 		);
 	}
 
@@ -116,14 +123,11 @@ class BoardTest {
 	@DisplayName("보드판 위에 폰이 주어졌을때 특정한 위치로 이동한다")
 	@ParameterizedTest
 	@MethodSource(value = "validPawnMoveLocations")
-	void givenPawn_whenMovePiece_thenReturnMovedPiece(Piece piece, String src, String dst, Piece expected) {
+	void givenPawn_whenMovePiece_thenReturnMovedPiece(Piece[] pieces, Location src, Location dst, Piece expected) {
 		// given
-		PieceMovable board = Board.init(repository, piece);
-
-		Location srcLocation = Location.from(src);
-		Location dstLocation = Location.from(dst);
+		PieceMovable board = Board.init(repository, pieces);
 		// when
-		Piece actual = board.movePiece(srcLocation, dstLocation).orElseThrow();
+		Piece actual = board.movePiece(src, dst).orElseThrow();
 		// then
 		assertThat(actual).isEqualTo(expected);
 		assertThat(repository.contains(actual)).isTrue();
