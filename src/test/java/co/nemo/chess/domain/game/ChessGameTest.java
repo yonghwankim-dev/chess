2e7f86d0ddede6c60afb8959eb9c8d1f9e45c898
yonghwankim-dev <dragonbead95@naver.com>
2024-12-14 16:58:18 +0900

    test: 폰 승급 테스트 추가

--- src/test/java/co/nemo/chess/domain/game/ChessGameTest.java
+++ src/test/java/co/nemo/chess/domain/game/ChessGameTest.java
@@ -1,5 +1,8 @@
 package co.nemo.chess.domain.game;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,6 +25,33 @@ class ChessGameTest {
 		game.startGame();
 		// then
 		AbstractChessPiece expected = PieceFactory.getInstance().darkPawn("a7");
+		Assertions.assertThat(board.getAllPieces())
+			.contains(expected);
+	}
+
+	@DisplayName("백 플레이어가 폰을 룩으로 승급시킨다")
+	@Test
+	void givenWhitePlayer_whenPawnMovedEndOfRank_thenPawnPromoRook() {
+		// given
+		String input = Stream.of(
+				"move a2 a4",
+				"move b7 b5",
+				"move a4 b5",
+				"move a7 a6",
+				"move b5 b6",
+				"move a6 a5",
+				"move b6 b7",
+				"move a5 a4",
+				"move b7 b8"
+			)
+			.collect(Collectors.joining(System.lineSeparator()));
+		InputStrategy inputStrategy = new StringInputStrategy(input);
+		Board board = Board.empty();
+		ChessGame game = new ChessGame(board, inputStrategy, ConsoleOutputStrategy.getInstance());
+		// when
+		game.startGame();
+		// then
+		AbstractChessPiece expected = PieceFactory.getInstance().whiteRook("b8");
 		Assertions.assertThat(board.getAllPieces())
 			.contains(expected);
 	}
